cd944c7c071fb61d870e7dca2734993d67094bf4
Farhan Mohammed <farhanhm12@gmail.com>
2021-01-24 16:22:44 -0500

    Added footer/header files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import Intro from './Sections/S01Intro';
-import Rules from './Sections/S02Rules';
-import About from './Sections/S03About';
-import FAQ from './Sections/S04FAQ';
-import Sponsors from './Sections/S05Sponsors';
+import React, { Component } from "react";
+import Footer from "Sections/Footer";
+import Header from "Sections/Header";
+import Intro from "./Sections/S01Intro";
+import Rules from "./Sections/S02Rules";
+import About from "./Sections/S03About";
+import FAQ from "./Sections/S04FAQ";
+import Sponsors from "./Sections/S05Sponsors";
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <Intro />
                 <Rules />
                 <About />
                 <FAQ />
                 <Sponsors />
+                <Footer />
             </div>
         );
     }
