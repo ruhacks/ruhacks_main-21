ecd7554b1fb7a7cd0e57620a9c3d40365eb9c2b1
Farhan <farhanhm12@gmail.com>
2021-02-03 05:34:51 -0500

    Finished Team
    There's still room for improvement

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import teammembers from 'values/team';
 export default class TeamList extends Component {
+    state = {
+        hover: false,
+    };
     render() {
         return (
             <div className="tm-con">
                 {teammembers.map(({ group, members }) => (
                     <div className="tm-row">
                         <div className="tm-group">{group}</div>
-                        {members.map(({ name, description, personComponent }) => (
-                            <div className="tm-person">
+                        {members.map(({ name, personComponent, description }) => (
+                            <div
+                                className={`tm-person ${
+                                    this.state.hover ? 'tm-person__small' : ''
+                                }`}
+                                onMouseEnter={() => this.setState({ hover: 1 })}
+                                onMouseLeave={() => this.setState({ hover: false })}
+                            >
                                 <div
                                     alt={name}
                                     className="tm-person__image"
                                     style={{ backgroundImage: `url(${personComponent})` }}
                                 />
-                                <div className="tm-person__name">{name}</div>
-                                <div className="tm-person__description">{description}</div>
+                                <div className="tm-person__text">
+                                    <div className="tm-person__name">{name}</div>
+                                    <div className="tm-person__description">{description}</div>
+                                </div>
                             </div>
                         ))}
                     </div>
@@ -23,4 +34,24 @@ export default class TeamList extends Component {
             </div>
         );
     }
+}
+function PersonComponent(props) {
+    let { name, description, personComponent } = props;
+    const [hovered, setHovered] = useState(false);
+    const toggleHover = () => setHovered(!hovered);
+    return (
+        <div
+            className="tm-person"
+            onMouseEnter={() => this.setState({ hover: 1 })}
+            onMouseLeave={() => this.setState({ hover: false })}
+        >
+            <div
+                alt={name}
+                className="tm-person__image"
+                style={{ backgroundImage: `url(${personComponent})` }}
+            />
+            <div className="tm-person__name">{name}</div>
+            <div className="tm-person__description">{description}</div>
+        </div>
+    );
 }
